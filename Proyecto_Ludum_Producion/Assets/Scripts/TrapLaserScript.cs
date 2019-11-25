bda1e542376dfc4cd91d5faee5ff5d9932a49830
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2019-11-25 21:48:31 +0100

    Laser Logic + Laser Beam

---
+++ Proyecto_Ludum_Producion/Assets/Scripts/TrapLaserScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapLaserScript : MonoBehaviour
+{
+
+    public SpriteRenderer mySprite;
+    public int NumberOfLasers;
+    public GameObject myLaserPrefab;
+
+    private int state = 0;
+
+    void Start()
+    {
+ 
+    }
+
+    public void InstanceLasers()
+    {
+        GameObject lastLaser = null;
+        GameObject aux = null;
+        for (int i = 0; i<=NumberOfLasers; i++)
+        {
+            if (aux == null)
+            {
+                lastLaser = Instantiate(myLaserPrefab, transform.position, transform.rotation);
+                aux = lastLaser;
+            }
+            else
+            {
+                lastLaser = Instantiate(myLaserPrefab, aux.transform.GetChild(2).position, aux.transform.GetChild(2).rotation);
+                aux = lastLaser;
+            }
+        }
+        state++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case 0: //appear
+                state++;
+                break;
+            case 1: //start blasting
+                InstanceLasers();
+                break;
+        }
+    }
+}
