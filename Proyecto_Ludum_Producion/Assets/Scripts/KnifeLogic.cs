5e64a06fcd685dc0f2ea811b75ecc0212d3e1a7e
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2019-11-26 17:25:47 +0100

    Knife Spawner

---
+++ Proyecto_Ludum_Producion/Assets/Scripts/KnifeLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeLogic : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    private Vector2 parentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentPosition = transform.parent.transform.position;
+        InvokeRepeating("Autodestroy", 5.0f, 1);
+    }
+
+    public void Autodestroy()
+    {
+       // Destroy(transform.parent.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        parentPosition.y = parentPosition.y - Speed;
+        transform.parent.transform.position = parentPosition;
+    }
+}
---
+++ Proyecto_Ludum_Producion/Assets/Scripts/KnifeSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeSpawner : MonoBehaviour
+{
+
+    public int NumberOfKnifes = 10;
+    public float Rate = 0.1f;
+    public float Speed = 5.0f;
+    public bool NegativeX = false;
+    public bool MakeItRain = false;
+    public GameObject Knife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    public void SpawnKnife()
+    {
+        Instantiate(Knife, transform.parent.transform.position, transform.parent.transform.rotation);
+    }
+
+    public void MovementX()
+    {
+        Vector2 _newPosition = new Vector2(transform.parent.transform.position.x, transform.parent.transform.position.y);
+        if (NegativeX)
+        {
+            _newPosition.x -= Mathf.Sin(Time.time) * Time.deltaTime * Speed;
+        }
+        else
+        {
+            _newPosition.x += Mathf.Sin(Time.time) * Time.deltaTime * Speed;
+        }
+        transform.parent.transform.position = _newPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MakeItRain)
+        {
+            Instantiate(Knife, transform.parent.transform.position, transform.parent.transform.rotation);
+        }
+        MovementX();
+    }
+}
