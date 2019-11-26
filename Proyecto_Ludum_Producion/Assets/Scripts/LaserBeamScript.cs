9c640e0a61c446d477aa1b0f5dd331bbe026ded8
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2019-11-26 16:55:03 +0100

    Laser Done with all Logic

--- Proyecto_Ludum_Producion/Assets/Scripts/LaserBeamScript.cs
+++ Proyecto_Ludum_Producion/Assets/Scripts/LaserBeamScript.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class LaserBeamScript : MonoBehaviour
 {
 
-    public SpriteRenderer MySprite;
+    public float TimeAlive = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("Autodestroy", 5.0f,1);
+    }
+    public void Autodestroy()
+    {
+        Destroy(transform.parent.gameObject);
     }
     private void OnTriggerEnter2D(Collider2D collision) //trigger to kill Player
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
 
--- Proyecto_Ludum_Producion/Assets/Scripts/TrapLaserScript.cs
+++ Proyecto_Ludum_Producion/Assets/Scripts/TrapLaserScript.cs
@@ -9,6 +9,13 @@ public class TrapLaserScript : MonoBehaviour
     public int NumberOfLasers;
     public GameObject myLaserPrefab;
 
+    public float Speed = 5.0f;
+    public bool MoveX = false;
+    public bool NegativeX = false;
+    public bool MoveY = false;
+    public bool NegativeY = false;
+
+
     private int state = 0;
 
     void Start()
@@ -25,15 +32,45 @@ public class TrapLaserScript : MonoBehaviour
             if (aux == null)
             {
                 lastLaser = Instantiate(myLaserPrefab, transform.position, transform.rotation);
+                lastLaser.transform.SetParent(transform.parent.transform);
                 aux = lastLaser;
             }
             else
             {
                 lastLaser = Instantiate(myLaserPrefab, aux.transform.GetChild(2).position, aux.transform.GetChild(2).rotation);
+                lastLaser.transform.SetParent(transform.parent.transform);
                 aux = lastLaser;
             }
         }
         state++;
+    }
+
+    public void MovementX()
+    {
+        Vector2 _newPosition = new Vector2(transform.parent.transform.position.x, transform.parent.transform.position.y);
+        if (NegativeX)
+        {
+            _newPosition.x -= Mathf.Sin(Time.time) * Time.deltaTime * Speed;
+        }
+        else{
+            _newPosition.x += Mathf.Sin(Time.time) * Time.deltaTime * Speed;
+        }
+        transform.parent.transform.position = _newPosition;
+    }
+
+    public void MovementY()
+    {
+        Vector2 _newPosition = new Vector2(transform.parent.transform.position.x, transform.parent.transform.position.y);
+        if (NegativeY)
+        {
+            _newPosition.y -= Mathf.Sin(Time.time) * Time.deltaTime * Speed;
+        }
+        else
+        {
+            _newPosition.y += Mathf.Sin(Time.time) * Time.deltaTime * Speed;
+        }
+
+        transform.parent.transform.position = _newPosition;
     }
 
     // Update is called once per frame
@@ -46,6 +83,16 @@ public class TrapLaserScript : MonoBehaviour
                 break;
             case 1: //start blasting
                 InstanceLasers();
+                break;
+            case 2:
+                if (MoveX)
+                {
+                    MovementX();
+                }
+                if (MoveY)
+                {
+                    MovementY();
+                }
                 break;
         }
     }
