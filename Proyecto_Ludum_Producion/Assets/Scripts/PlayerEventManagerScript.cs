5d122ccc517a12d294336aa55730e65ffffb6b9b
Javi <javiergarciagomez@enti.cat>
2019-11-27 12:59:30 +0100

    Player change speed event done

--- Proyecto_Ludum_Producion/Assets/Scripts/PlayerEventManagerScript.cs
+++ Proyecto_Ludum_Producion/Assets/Scripts/PlayerEventManagerScript.cs
@@ -5,38 +5,73 @@ using UnityEngine;
 public class PlayerEventManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Public vars
     public float zoomSpeed = 1;
     public float targetOrtho;
     public float smoothSpeed = 2.0f;
     public float minOrtho = 1.0f;
     public float maxOrtho = 5.0f;
-    public bool zoomIn = false;
+    public bool enableEvent = false;
+    public enum events {ZOOMIN,SPEEDDEBUFF,SPEEDBUFF };
 
     public Transform _camera;
     public Vector3 offset;
 
+    public float debuffSpeed;
+    public float buffSpeed;
+
+    private float defaultSpeed;
+
+
     void Start()
     {
         targetOrtho = Camera.main.orthographicSize;
+        defaultSpeed = this.transform.parent.GetComponent<PlayerController>().speed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (zoomIn)
+        
+        if(enableEvent)
+            eventManager(events.SPEEDBUFF);
+        else
         {
-            targetOrtho -=  zoomSpeed;
-            targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
-            _camera.position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, _camera.position.z);
 
-        }
-        else if(!zoomIn)
-        {
-            targetOrtho +=  zoomSpeed;
+            targetOrtho += zoomSpeed;
             targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+            this.transform.parent.GetComponent<PlayerController>().speed = defaultSpeed;
         }
-
         Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+
+        
+    }
+
+    public void eventManager(events eventNumber)
+    {
+       
+        switch (eventNumber)
+        {
+            case events.ZOOMIN:
+                targetOrtho -= zoomSpeed;
+                targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+                _camera.position = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, _camera.position.z);
+
+                this.transform.parent.GetComponent<PlayerController>().speed = defaultSpeed;
+                break;
+            case events.SPEEDDEBUFF:
+                this.transform.parent.GetComponent<PlayerController>().speed = debuffSpeed;
+
+                targetOrtho += zoomSpeed;
+                targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+                break;
+            case events.SPEEDBUFF:
+                this.transform.parent.GetComponent<PlayerController>().speed = buffSpeed;
+
+                targetOrtho += zoomSpeed;
+                targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+                break;
+
+        }
     }
 }
