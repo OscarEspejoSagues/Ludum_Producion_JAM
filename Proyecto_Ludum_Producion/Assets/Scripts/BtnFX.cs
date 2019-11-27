9f1fcfd7672cbff23a0b2ee9953d38c0129e200f
Martigomezprunera <magopru1995@gmail.com>
2019-11-27 11:13:25 +0100

    Sound UI Menu (Hover + Pressed)

---
+++ Proyecto_Ludum_Producion/Assets/Scripts/BtnFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnFX : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+    private float volume;
+
+    private void Start()
+    {
+        myFx.volume = 0.1f;
+    }
+
+    public void HoverButton()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+
+    public void PressedButton()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
