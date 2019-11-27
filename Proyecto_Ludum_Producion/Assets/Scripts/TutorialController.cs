2bcac5f2c4a04cf122b015d3dceb8a2f63a5d753
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2019-11-27 11:55:51 +0100

    Start Tutorial Level

---
+++ Proyecto_Ludum_Producion/Assets/Scripts/TutorialController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public GameObject Arrow;
+    public GameObject DeathTrap;
+    public SpriteRenderer TextImage;
+    public Sprite[] TextToShow;
+    public float Rate = 5.0f;
+
+    private int CurrentText = 0;
+    private float nextActionTime = 5.0f;
+
+    private void Awake()
+    {
+        DeathTrap.SetActive(false);
+    }
+
+    void Start()
+    {
+        TextImage.sprite = TextToShow[CurrentText];
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += Rate;
+            CurrentText++;
+            TextImage.sprite = TextToShow[CurrentText];
+            switch (CurrentText)
+            {
+                case 1:
+                    transform.parent.position = new Vector3(8.0f, -1.12f, 1.0f);
+                    break;
+                case 2:
+                    Arrow.SetActive(false);
+                    transform.parent.position = new Vector3(0.77F, -1.17f, 1.0f);
+                    break;
+                case 6:
+                    DeathTrap.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
---
+++ Proyecto_Ludum_Producion/Assets/Scripts/TutorialDeathTrap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDeathTrap : MonoBehaviour
+{
+
+    private Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.y = transform.position.y - 0.05f;
+        transform.transform.position = pos;
+    }
+}
